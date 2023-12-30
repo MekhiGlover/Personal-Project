bb7c0760c47fe57c2ff3320e1fa9457f1dc4e4ec
Mekhi <mekhi.j.glover@gmail.com>
2023-12-30 09:25:18 -0500

    creating the navbar

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request,redirect
+import math
+import random
 app = Flask(__name__)
 
+Users = []
 
 
-#displays index page
+
+class User:
+    def __init__(self,username,email,password):
+        self.username = username,
+        self.email = email,
+        self.password = password
+        self.user_id = math.ceil(random())
+
+
+
+
+
+# CREATING ROUTES BETWEEN ALL PAGES
+
 @app.get('/')
 def index():
     return render_template('index.html')
-
 
 @app.get('/page2')
 def second():
@@ -17,3 +31,19 @@ def second():
 @app.get('/page3')
 def third():
     return render_template('p3.html')
+
+
+# CREATING ROUTES TO LOG IN AND OUT 
+
+
+
+@app.post('/signup/<string:username>/<string:email>/<string:password>')
+def create(username,email,password):
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    newUser = User.__init__(username=username, email = email, password=password)
+    Users.append(newUser)
+    for person in Users:
+        print(person)
+    return redirect('/')
